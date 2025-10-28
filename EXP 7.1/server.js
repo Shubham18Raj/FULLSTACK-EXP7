67a2e30b1778b7b05e442c300f215352ec1345ac
Shubham18Raj <shubhamsingh45689@gmail.com>
2025-10-28 22:26:38 +0530

    Update server.js

--- EXP 7.1/server.js
+++ EXP 7.1/server.js
@@ -1,1 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+
+const products = [
+  { id: 1, name: "Laptop", price: 75000 },
+  { id: 2, name: "Smartphone", price: 30000 },
+  { id: 3, name: "Headphones", price: 2500 },
+];
+
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
