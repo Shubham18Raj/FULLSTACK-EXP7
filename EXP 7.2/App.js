68de3c8362f4feaf0ac9a962042c8e9f51c428d2
Shubham18Raj <shubhamsingh45689@gmail.com>
2025-10-28 22:45:34 +0530

    Update App.js

--- EXP 7.2/App.js
+++ EXP 7.2/App.js
@@ -1,1 +1,15 @@
+import React from "react";
+import ProductList from "./components/ProductList";
+import Cart from "./components/Cart";
+
+const App = () => {
+  return (
+    <div style={{ display: "flex", justifyContent: "space-around" }}>
+      <ProductList />
+      <Cart />
+    </div>
+  );
+};
+
+export default App;
 
