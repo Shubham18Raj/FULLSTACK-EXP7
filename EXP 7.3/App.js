6e84387c4b192dc2f58d1579851be85e84042aa7
Shubham18Raj <shubhamsingh45689@gmail.com>
2025-10-28 22:52:52 +0530

    Update App.js

--- EXP 7.3/App.js
+++ EXP 7.3/App.js
@@ -1,1 +1,11 @@
+import React from "react";
+import Chat from "./components/Chat";
+
+export default function App() {
+  return (
+    <div style={{ fontFamily: "Arial, sans-serif" }}>
+      <Chat />
+    </div>
+  );
+}
 
